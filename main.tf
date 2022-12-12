8857eb9dc40c99fe584d5ec1341ad7e313bae7ad
Evgeniy Nikolskiy <kniginrd@mail.ru>
2022-12-12 03:13:32 +0300

    bucket

--- main.tf
+++ main.tf
@@ -7,30 +7,32 @@ terraform {
   required_version = ">= 0.13"
 }
 
+               # Centos 7 image
+
+data "yandex_compute_image" "centos7" {
+  family = "centos-7"
+}
+
 provider "yandex" {
   token     = "AQAAAAAS9j4ZAATuwaB6EWjQVE5PuHLDBOmXiaI"
   cloud_id  = "b1gm53rrhubfia3qpp2g"
   folder_id = "b1g6i02oni8ft6c24ped"
-  zone      = "ru-central1-a"
-}
-
-               # Создание статического ключа доступа
-
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  service_account_id = "ajec54kmcc4gi78eqhuh"
-  description        = "static access key for object storage"
+  zone      = "ru-central1-b"
 }
 
 data "terraform_remote_state" "vpc" {
-  backend = "?"
+  backend = "s3"
 
   config = {
     endpoint   = "storage.yandexcloud.net"
     region     = "ru-central1"
-    access_key = "yandex_iam_service_account_static_access_key.sa-static-key.access_key"
-    secret_key = "yandex_iam_service_account_static_access_key.sa-static-key.secret_key"
+    access_key = "YCAJEdWj_znXM1jI2-oQzyKpE"
+    secret_key = "YCPAo9koLP51oMQ-Ll3Z11KuSxFRkE6iwFxDuTP3"
     bucket     = "diplom-netology-bucket"
     key        = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
   }
 }
 
@@ -44,7 +46,7 @@ resource "yandex_compute_instance" "vm-4" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd87va5cc00gaq2f5qfb"
+      image_id = data.yandex_compute_image.centos7.id
     }
   }
 
